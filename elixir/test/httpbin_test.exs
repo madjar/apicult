8749ac2bcc2dba96622ed66cb851d6e75942f485
Georges Dubus <georges.dubus@soundcloud.com>
2022-04-13 16:29:00 +0200

    Some more endpoints for httpbin
    This were added to debug a bug that was not there :)

--- elixir/test/httpbin_test.exs
+++ elixir/test/httpbin_test.exs
@@ -9,6 +9,14 @@ defmodule HttpbinTest do
     assert Httpbin.example_get("test_value").args.qs == "test_value"
   end
 
+  test "A post with json works" do
+    assert Httpbin.post_json("test_value").json.key == "test_value"
+  end
+
+  test "A post with form works" do
+    assert Httpbin.post_form("test_value").form.key == "test_value"
+  end
+
   test "A redirect is an exception" do
     assert_raise Apicult.Request.NonSuccessResponseError, &Httpbin.redirect/0
   end
