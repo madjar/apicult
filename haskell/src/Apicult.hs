56ecdc89b4e8f2e6d4c0f50d6a45e05557f0e877
Georges Dubus <georges.dubus@micropsi-industries.com>
2022-12-09 17:38:52 +0100

    Export makeApi

--- haskell/src/Apicult.hs
+++ haskell/src/Apicult.hs
@@ -1,15 +1,8 @@
-{- |
-Copyright: (c) 2022 Georges Dubus
-SPDX-License-Identifier: MIT
-Maintainer: Georges Dubus <georges.dubus@hey.com>
+-- |
+-- Copyright: (c) 2022 Georges Dubus
+-- Maintainer: Georges Dubus <georges.dubus@hey.com>
+--
+-- See README for more info
+module Apicult (makeApi) where
 
-See README for more info
--}
-
-module Apicult
-    ( projectName
-    ) where
-
-
-projectName :: String
-projectName = "apicult"
+import Apicult.TH (makeApi)
